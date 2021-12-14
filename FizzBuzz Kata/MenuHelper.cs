2b703485563a000192e423d950de2ce38e83ba24
phillippelevidad <phillippelevidad@gmail.com>
2021-12-14 19:55:27 -0300

    Better exercise name in menus

--- FizzBuzz Kata/MenuHelper.cs
+++ FizzBuzz Kata/MenuHelper.cs
@@ -13,7 +13,8 @@ namespace FizzBuzz_Kata
 
             foreach (var entry in exerciseManager.KnownExercises)
             {
-                Console.WriteLine($"| {entry.Key} - {entry.Value}");
+                var exerciseName = GetExerciseName(entry.Value);
+                Console.WriteLine($"| {entry.Key} - {exerciseName}");
             }
 
             Console.WriteLine("| ... Or type any other number to exit");
@@ -24,6 +25,19 @@ namespace FizzBuzz_Kata
         {
             Console.WriteLine("\n\nPress enter to return to the menu.");
             Console.ReadLine();
+        }
+
+        private static string GetExerciseName(Type exerciseType)
+        {
+            const string suffix = "Exercise";
+            var name = exerciseType.Name;
+
+            if (name.EndsWith(suffix))
+            {
+                name = name[0..^suffix.Length];
+            }
+
+            return name;
         }
     }
 }
