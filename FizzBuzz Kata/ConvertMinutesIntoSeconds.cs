4b26656f551db015299dbaefd2c664a483241e4b
phillippelevidad <phillippelevidad@gmail.com>
2021-12-14 19:19:17 -0300

    ConvertMinutesIntoSeconds moved

---
+++ FizzBuzz Kata/ConvertMinutesIntoSeconds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzz_Kata
+{
+    public class ConvertMinutesIntoSeconds : IExercise
+    {
+        public void Run()
+        {
+            Console.WriteLine("Convert Minutes into Seconds.\n" +
+            "Function that takes an integer minutes and converts it to seconds.\n" +
+            "Examples:\n" +
+            "convert(5) = 300\n" +
+            "convert(3) = 180\n" +
+            "convert(2) = 120\n" +
+            "\n-------------------------------- \n\n");
+
+            Console.Write("Write an integer to represent the minutes: \n>>> ");
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = minutes * 60;
+            Console.WriteLine("\nThe minute(s): [" + minutes + "] is (are) equal to: [" + seconds + "] Seconds.");
+
+            MenuHelper.PrintMenu();
+        }
+    }
+}
